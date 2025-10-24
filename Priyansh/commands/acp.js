776978022521ff20d68251e30e205778e054535a
attaullahkh635-source <attaullahkh635@gmail.com>
2025-10-24 10:39:19 +0500

    Update acp.js

--- Priyansh/commands/acp.js
+++ Priyansh/commands/acp.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "acp",
   version: "1.0.0",
   hasPermssion: 2,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "𝗔𝘁𝘁𝗮𝘂𝗹𝗹𝗮𝗵",
   description: "Make friends via Facebook id",
   commandCategory: "bot id",
   usages: "uid",
