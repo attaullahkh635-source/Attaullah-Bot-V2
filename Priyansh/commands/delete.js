0a85755ee40bddd710608b69e8358131cb1b85a3
attaullahkh635-source <attaullahkh635@gmail.com>
2025-10-24 12:04:05 +0500

    Update delete.js

--- Priyansh/commands/delete.js
+++ Priyansh/commands/delete.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "delete",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝗔𝘁𝘁𝗮𝘂𝗹𝗹𝗮𝗵 𝗞𝗵𝘂𝗵𝗮𝗿𝗼",
 	description: "Delete 🖱️",
 	commandCategory: "Image",
 	usages: "rank",
