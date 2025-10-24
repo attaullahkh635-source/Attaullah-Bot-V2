0760848f8c8fa352f2677e174461165074975e18
attaullahkh635-source <attaullahkh635@gmail.com>
2025-10-24 12:14:34 +0500

    Update rule.js

--- Priyansh/commands/rule.js
+++ Priyansh/commands/rule.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "rule",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝗔𝘁𝘁𝗮𝘂𝗹𝗹𝗮𝗵",
 	description: "Customize the law for each group",
 	commandCategory: "Box Chat",
 	usages: "[add/remove/all] [content/ID]",
