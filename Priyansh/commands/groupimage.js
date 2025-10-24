959bec149e28c82219fc5ffe0831d228ac66c6a1
attaullahkh635-source <attaullahkh635@gmail.com>
2025-10-24 12:12:31 +0500

    Update groupimage.js

--- Priyansh/commands/groupimage.js
+++ Priyansh/commands/groupimage.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	name: "groupimage",
 	version: "1.0.0", 
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "𝗔𝘁𝘁𝗮𝘂𝗹𝗹𝗮𝗵",
 	description: "Change your group image",
 	commandCategory: "Box", 
 	usages: "groupimage", 
